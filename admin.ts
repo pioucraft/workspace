dc9c18c09a22bca767f97c9d8cafc1a97a39ba41
Nathan <hello@gougoule.ch>
2025-06-15 22:00:04 +0200

    Make the admin panel akshually work

---
+++ admin.ts
@@ -0,0 +1,68 @@
+import { user } from "./src/lib/server/db/schema";
+
+import { drizzle } from 'drizzle-orm/postgres-js';
+import * as schema from './src/lib/server/db/schema';
+import postgres from 'postgres';
+import { eq } from "drizzle-orm";
+
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+const client = postgres(process.env.DATABASE_URL);
+
+export const db = drizzle(client, { schema });
+
+console.log("Welcome to the Admin Panel");
+
+console.log("Available Commands:");
+console.log("1. Add User");
+console.log("2. Remove User");
+console.log("3. View Users");
+
+let command = prompt("Enter a command (1-3): ");
+switch (command) {
+    case '1':
+        addUser();
+        break;
+    case '2':
+        console.log("Removing a user...");
+        removeUser();
+        break;
+    case '3':
+        console.log("Viewing all users...");
+        viewUsers();
+        break;
+    default:
+        console.log("Invalid command. Please try again.");
+}
+
+async function addUser() {
+    let username = prompt("Enter username: ") ?? "";
+    let password = prompt("Enter password: ") ?? "";
+
+    let passwordHash = await Bun.password.hash(password)
+
+    await db.insert(user).values({
+        name: username,
+        passwordHash: passwordHash,
+    });
+    
+    console.log(`User ${username} added successfully.`); 
+    process.exit(0);
+}
+
+async function viewUsers() {
+    const users = await db.select().from(user);
+    console.log("Current Users:");
+    users.forEach(u => {
+        console.log(`- username: ${u.name} with id: ${u.id}`);
+    });
+    process.exit(0);
+}
+
+async function removeUser() {
+    let id = prompt("Enter user ID to remove: ") ?? "";
+    await db.delete(user).where(eq(user.id, id));
+    console.log(`User with ID ${id} removed successfully.`);
+    process.exit(0);
+}
+
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,7 @@
 import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
-    id: uuid("id").primaryKey(),
+    id: uuid('id').defaultRandom().primaryKey(),
     name: text("name").notNull(),
     passwordHash: text("password_hash").notNull(),
 });
