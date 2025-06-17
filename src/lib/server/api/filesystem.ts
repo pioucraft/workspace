7410c07ac5fd3a598416adb5a558e8c4014d22fa
Nathan Gasser <hello@gougoule.ch>
2025-06-17 21:43:29 +0200

    Make the filesystem work

--- src/lib/server/api/filesystem.ts
+++ src/lib/server/api/filesystem.ts
@@ -1,13 +1,15 @@
-import { statSync, readFileSync, readdirSync } from 'fs';
+import { statSync, readFileSync, readdirSync, writeFileSync, renameSync, mkdirSync } from 'fs';
 
 export function readFileOrFolder(path: string): {fileContent?: string, folderContent?: {name: string, stats: ReturnType<typeof statSync>}[]} {
-    const stats = statSync(path)
+    let formatedPath = "./data" + path
+
+    const stats = statSync(formatedPath);
     if(stats.isFile()) {
-        const fileContent = readFileSync(path, 'utf-8');
+        const fileContent = readFileSync(formatedPath, 'utf-8');
         return { fileContent };
 
     } else if (stats.isDirectory()) {
-        const folder = readdirSync(path);
+        const folder = readdirSync(formatedPath);
         let folderContent: {name: string, stats: ReturnType<typeof statSync>}[] = [];
 
         for (const item of folder) {
@@ -22,3 +24,27 @@ export function readFileOrFolder(path: string): {fileContent?: string, folderCon
 
 }
 
+export function modifyFileContent(path: string, content: string) {
+    let formatedPath = "./data" + path;
+
+    writeFileSync(formatedPath, content, 'utf-8')
+}
+
+export function moveFileOrFolder(source: string, destination: string) {
+    let formatedSource = "./data" + source;
+    let formatedDestination = "./data" + destination;
+
+    renameSync(formatedSource, formatedDestination);
+}
+
+export function createFile(path: string) {
+    let formatedPath = "./data" + path;
+
+    writeFileSync(formatedPath, '', 'utf-8');
+}
+
+export function createFolder(path: string) {
+    let formatedPath = "./data" + path;
+
+    mkdirSync(formatedPath, { recursive: true });
+}
