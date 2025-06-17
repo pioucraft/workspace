8c1c3908ca73bf1f51578d70ac98948c1ffd0b8f
Nathan Gasser <hello@gougoule.ch>
2025-06-17 12:30:05 +0200

    Add hemm a log in page (only design), yeah

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,36 @@
+<div class="w-screen h-screen flex gap-5 items-center justify-center bg-[#f4f5f7]">
+    <div class="w-1/2 h-full flex justify-center items-center">
+          <div class="gap-2 flex flex-col">
+            <h1 class="text-3xl font-bold">Welcome back !</h1>
+            <p class="text-2xl font-semibold">Please sign in to continue</p>
+        </div>
+        <div class="absolute mb-[15%] ml-[22%] w-24 h-24 bg-gray-200/40 rounded-full"></div>
+        <div class="absolute mt-[13%] mr-[27%] w-16 h-16 bg-gray-300/50 rounded-full"></div>
+        <div class="absolute mb-[20%] mr-[16%] w-32 h-32 bg-gray-200/30 rounded-full"></div>
+        <div class="absolute mt-[23%] ml-[20%] w-20 h-20 bg-gray-300/40 rounded-full"></div>
+        <div class="absolute mb-[10%] ml-[10%] w-12 h-12 bg-gray-200/50 rounded-full"></div>
+        <div class="absolute mt-[5%] mr-[10%] w-8 h-8 bg-gray-300/60 rounded-full"></div>
+        <div class="absolute mb-[30%] mr-[5%] w-16 h-16 bg-gray-200/40 rounded-full"></div>
+        <div class="absolute mt-[30%] ml-[5%] w-24 h-24 bg-gray-300/50 rounded-full"></div>
+    </div>
+    <div class="w-1/2 h-full bg-white flex flex-col justify-center items-center gap-5">
+        <h1 class="text-3xl font-bold">Sign in</h1>
+        <p>Enter your login credentials to continue :</p>
+        <div class="mt-8 w-[420px]">
+            <p>Username</p>
+            <input type="text" placeholder="Enter your username" class="w-full h-10 border border-gray-300 rounded-full p-5 pl-3 mt-1 focus:outline-none focus:ring-2 focus:ring-slate-500" />
+        </div>
+        <div class="w-[420px]">
+            <p>Password</p>
+            <input type="password" placeholder="Enter your password" class="w-full h-10 border border-gray-300 rounded-full p-5 pl-3 mt-1 focus:outline-none focus:ring-2 focus:ring-slate-500" />
+        </div>
+        <button class="w-[420px] h-10 bg-slate-600 cursor-pointer rounded-full text-white font-semibold mt-5 hover:bg-slate-500 transition-colors duration-300">
+            Sign in
+        </button>
+    </div>
+</div>
+
+<style>
+
+
+</style>
