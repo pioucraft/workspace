0f49d8b4094e3868d146db813b25bf15f8143875
Nathan Gasser <hello@gougoule.ch>
2025-06-17 18:06:18 +0200

    Make the log in page better

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,34 +1,47 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-    var password = '';
-    var username = '';
+	var password = '';
+	var username = '';
 
-    async function login() {
-        if (username === '' || password === '') {
-            alert('Please fill in both fields.');
-            return;
-        }
+	async function login() {
+		if (username === '' || password === '') {
+			alert('Please fill in both fields.');
+			return;
+		}
 
-        try {
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, password })
-            });
+		try {
+			const response = await fetch('/api/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ username, password })
+			});
 
-            if (!response.ok) {
-                throw new Error('Login failed');
-            }
-            
-            goto("/")
-        } catch (error) {
-            console.error('Error:', error);
-            alert('Login failed. Please try again.');
-        }
-    }
+			if (!response.ok) {
+				throw new Error('Login failed');
+			}
+
+			goto('/');
+		} catch (error) {
+			console.error('Error:', error);
+			alert('Login failed. Please try again.');
+		}
+	}
+
+	onMount(() => {
+		window.addEventListener('keydown', (event) => {
+			if (event.key === 'Enter') {
+				if (document.activeElement?.id === 'username') {
+					document.getElementById('password')?.focus();
+				} else if (document.activeElement?.id === 'password') {
+					login();
+				}
+			}
+		});
+	});
 </script>
 
 <div class="bg-ctp-surface0 text-ctp-text flex h-screen w-screen items-center justify-center gap-5">
@@ -40,6 +53,7 @@
 		<div class="mt-8 w-[420px]">
 			<p class="mb-1">Username</p>
 			<input
+				id="username"
 				type="text"
 				bind:value={username}
 				class="bg-ctp-surface1 text-ctp-text focus:ring-ctp-peach h-10 w-full rounded-full px-4 transition-colors duration-300 focus:ring-2 focus:outline-none"
@@ -49,6 +63,7 @@
 		<div class="w-[420px]">
 			<p class="mb-1">Password</p>
 			<input
+				id="password"
 				type="password"
 				bind:value={password}
 				class="bg-ctp-surface1 text-ctp-text focus:ring-ctp-peach h-10 w-full rounded-full px-4 transition-colors duration-300 focus:ring-2 focus:outline-none"
